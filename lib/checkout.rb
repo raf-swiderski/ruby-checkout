30e965ea24eb9e0960fe9168a4237c3a3038fda7
Raf Swiderski <rafswid96@gmail.com>
2022-05-24 15:16:54 +0100

    apply discount an item for a promotion

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,31 +2,73 @@ require_relative 'item'
 
 class Checkout
 
-    attr_reader :total
+    attr_reader :total, :items
 
     def initialize
         @total = 0
+        @items = []
     end
 
     def total
+
         if @total == 0 
             return "Total price: £0.00"
         end
 
-        if @total >= 60
-
-            @total = @total * 0.90
-
+        if count_item("Lavender heart") >= 2
+            @total = drop_price_of_item(001, 8.50) # returns new @total
         end
 
-        return "Total price: £#{@total}"
+        recalculate_total
+        
+        if @total.to_i >= 6000 #10% discount if over £60.00
+            @total = @total.to_i * 0.90
+        end
+
+
+        @total = @total.to_i.round()
+        @total = (@total.to_f / 100)
+
+        print_message = "Total price: £#{@total}"
+
+        if print_message.split("")[-2] == "."
+            print_message += "0"
+        end
+
+        return print_message
+
     end
 
     def scan(item)
-        @total += item.price
+        @total += item.price * 100
+        @items << item
     end
 
+    def count_item(name) 
+        count = 0
+        for item in @items do
+            if item.name == name
+                count = count + 1
+            end
+        end
+        return count        
+    end
 
+    def drop_price_of_item(product_code, new_price)
+        @items.map do |item|
+            if item.product_code == product_code
+                item.price = new_price
+            end
+        end   
+    end
+
+    def recalculate_total
+        @total = 0
+        for item in @items do
+            @total += item.price * 100
+        end
+        return @total
+    end
 
 end
 
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -6,7 +6,7 @@ describe Checkout do
         @checkout = Checkout.new
         @item_001 = Item.new(001, "Lavender heart", 9.25)
         @item_002 = Item.new(002, "Personalised cufflinks", 45.00)
-        @item_003 = Item.new(003, "Lavender heart", 19.95)
+        @item_003 = Item.new(003, "Kids T-shirt", 19.95)
     end
 
     it "prints out the total" do
@@ -19,11 +19,29 @@ describe Checkout do
     end
 
     it "10% discount when spending over £60" do
+        @checkout.scan(@item_002)
+        @checkout.scan(@item_002)
+        expect(@checkout.total).to eq "Total price: £81.00"
+    end
+
+    it "10% discount when spending over £60" do
         @checkout.scan(@item_001)
         @checkout.scan(@item_002)
         @checkout.scan(@item_003)
         expect(@checkout.total).to eq "Total price: £66.78"
     end
 
+    it "if you buy 2 or more lavendar hearts the price of them drops to £8.50" do
+        @checkout.scan(@item_001)
+        @checkout.scan(@item_001)
+        expect(@checkout.total).to eq "Total price: £17.00"
+    end
+
+    it "if you buy 2 or more lavendar hearts the price of them drops to £8.50" do
+        @checkout.scan(@item_001)
+        @checkout.scan(@item_003)
+        @checkout.scan(@item_001)
+        expect(@checkout.total).to eq "Total price: £36.95"
+    end
 
 end
