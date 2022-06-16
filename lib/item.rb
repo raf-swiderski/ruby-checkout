76a2ed141a75f0126683c1739a71c5893d5cdd24
Raf Swiderski <rafswid96@gmail.com>
2022-06-16 12:24:12 +0100

    encapsulated the attributes of the item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,11 +1,16 @@
 class Item
 
-    attr_accessor :product_code, :name, :price
+    attr_reader :product_code, :name, :price
 
     def initialize(product_code, name, price)
         @product_code = product_code 
         @name = name
         @price = price
     end
+
+    def set_price(price)
+        @price = price
+    end
+
 
 end
